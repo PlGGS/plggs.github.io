ef155fd3c285b53e1b5a1f40984f6313ca54b563
Sebastian <sebastian@strempfer.com>
2018-04-03 21:47:52 -0500

    :a:

--- script/page-loader.js
+++ script/page-loader.js
@@ -1,6 +1,9 @@
-var page = window.location.hash.substr(0);
+var page = window.location.hash.substr(1);
+
+console.log(page)
 
 $.get("/sites/"+page+".html", function(data) {
+    console.log(data)
     $(function() {
         $("div.content").html(data);
     })
